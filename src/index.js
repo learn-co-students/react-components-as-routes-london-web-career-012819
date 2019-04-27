7f213d7290c7e48b8cb4039e6030afcc3b5892b7
stubrew24 <sewelly@gmail.com>
2019-04-27 10:17:29 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Login from './Login'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white'
+}
 
-const About = () => {
-  return (
-    <div>
-      <h1>This is my about component!</h1>
-    </div>
-  );
-};
+const Navbar = () => {
+  return (<div>
+    <NavLink to="/" exact style={link} activeStyle={{background: 'darkblue'}}>Home</NavLink>
+    <NavLink to="/about" exact style={link} activeStyle={{background: 'darkblue'}}>About</NavLink>
+    <NavLink to="/login" exact style={link} activeStyle={{background: 'darkblue'}}>Login</NavLink>
+  </div>)
+}
 
-const Login = () => {
-  return (
-    <div>
-      <form>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-          <label htmlFor="username">Username</label>
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-          <label htmlFor="password">Password</label>
-        </div>
-        <input type="submit" value="Login" />
-      </form>
-    </div>
-  );
-};
 
 ReactDOM.render((
   <Router>
       <React.Fragment>
-        <Route exact path="/" render={Home} />
-        <Route exact path="/about" render={About} />
-        <Route exact path="/login" render={Login} />
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/login" component={Login} />
       </React.Fragment>
   </Router>),
   document.getElementById('root')
